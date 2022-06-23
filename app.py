fcdb852de2574c77094dc2602d9680e2960bad7a
finkin <davis.frank@satis-information.de>
2022-06-24 01:17:14 +0200

    prediction

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import numpy as np
+import streamlit as st
+import polars as pl
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -22,6 +24,9 @@ import plotly.express as px
 
 import pickle
 
+MODEL_PATH = "data/log_reg.model"
+DATASET_PATH = "data/heart_2020_final.csv"
+
 def write_log(msg, prefix="process"):
     f = open("log/data.log", "a+")
     f.write("[" + str(prefix) + "]\t" + str(msg) + "\n")
@@ -29,10 +34,10 @@ def write_log(msg, prefix="process"):
     f.close()
 
 
-#print("hello world! Im learning about you!")
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.max_rows', None)
-data_csv = pd.read_csv('data/heart_2020_final.csv')
+# print("hello world! Im learning about you!")
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+data_csv = pd.read_csv(DATASET_PATH)
 data_csv.info()
 
 le = LabelEncoder()
@@ -52,7 +57,7 @@ data_csv[num_cols] = Scaler.fit_transform(data_csv[num_cols]) """
 figures = px.imshow(data_csv.corr(), color_continuous_scale="Blues")
 figures.update_layout(height=800)
 # if you like to see the color corr matrix
-figures.show()
+#figures.show()
 
 corr_matrix = data_csv.corr()
 corr_matrix["HeartDisease"].sort_values(ascending=False)
@@ -72,7 +77,6 @@ X_train_resampled, y_train_resampled, = ros.fit_resample(X_train, y_train)
 y_train_resampled.value_counts()
 
 
-
 LR = LogisticRegression()
 LR.fit(X_train_resampled, y_train_resampled)
 
@@ -82,4 +86,58 @@ print(classification_report(y_test, y_pred_6))
 sns.set_context("poster")
 dispo = plot_confusion_matrix(LR, x_test, y_test, colorbar=False)
 
-pickle.dump(LR, open("data/logistic_regression.model", 'wb'))
+pickle.dump(LR, open(MODEL_PATH, 'wb'))
+
+
+def get_dataframe(bmi_cat, smoking, alcohol_drink, stroke, phys_health, ment_health, diff_walk, sex, age_cat, race, diabetic, sleep_time, asthma, kid_dis, skin_canc):
+    features = pd.DataFrame({
+        "BMI": [bmi_cat],
+        "Smoking": [smoking],
+        "AlcoholDrinking": [alcohol_drink],
+        "Stroke": [stroke],
+        "PhysicalHealth": [phys_health],
+        "MentalHealth": [ment_health],
+        "DiffWalking": [diff_walk],
+        "Sex": [sex],
+        "AgeCategory": [age_cat],
+        "Race": [race],
+        "Diabetic": [diabetic],
+        "SleepTime": [sleep_time],
+        "Asthma": [asthma],
+        "KidneyDisease": [kid_dis],
+        "SkinCancer": [skin_canc]
+    })
+    return features
+
+
+@st.cache(persist=True)
+def load_dataset(path: str) -> pd.DataFrame:
+    heart_df = pl.read_csv(path)
+    heart_df = heart_df.to_pandas()
+    heart_df = pd.DataFrame(np.sort(heart_df.values, axis=0),
+                            index=heart_df.index,
+                            columns=heart_df.columns)
+    return heart_df
+
+
+dataset = load_dataset(DATASET_PATH)
+
+input_dataframe = get_dataframe(
+    3.0, 1, 0, 0, 30, 0, 1, 1, 8, 0, 1, 15, 1, 0, 0)
+df = pd.concat([input_dataframe, dataset], axis=0)
+df = df.drop(columns=["HeartDisease"])
+write_log("\n"+str(df.head()))
+
+df = df[:1]
+df.fillna(0, inplace=True)
+write_log("\n"+str(df.head()))
+log_model = pickle.load(open(MODEL_PATH, "rb"))
+
+prediction = log_model.predict(df)
+prediction_prob = log_model.predict_proba(df)
+print(str(prediction))
+if prediction == 0:
+    value = round(prediction_prob[0][1] * 100, 2)
+else:
+    value = round(prediction_prob[0][1] * 100, 2)
+print("probability that you will have a heart disease is "+str(value)+" %")
---
+++ readMe.md
@@ -0,0 +1,7 @@
+# Logistische Regression mit vorhersage Funktion für einen Datensatz
+
+- starten der Anwendung
+
+```
+python3 app.py
+```
